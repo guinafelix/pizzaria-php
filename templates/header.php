284e6056623b25e15a4cff6a978d1a5fb08b3331
guinafelix <oguilhermeleitefelix@gmail.com>
2023-04-15 19:05:37 -0300

    refactor: add session messages

--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,17 @@
+<?php
+  include("process/conn.php");
+
+  $msg = "";
+
+  if (isset($_SESSION["msg"])) {
+    $msg = $_SESSION["msg"];
+    $status = $_SESSION["status"];
+
+    $_SESSION["msg"] = "";
+    $_SESSION["status"] = "";
+
+  }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,6 +38,8 @@
       </a>
     </nav>
   </header>
-  <div class="alert alert-success">
-    <p>Pedido feito com sucesso!</p>
-  </div>
+  <?php if($msg != ""): ?>
+    <div class="alert alert-<?= $status ?>">
+      <p><?= $msg ?></p>
+    </div>
+  <?php endif; ?>
