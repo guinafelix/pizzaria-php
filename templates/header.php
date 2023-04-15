4fecffcf41e1d284532623c08b591087a83abe19
guinafelix <oguilhermeleitefelix@gmail.com>
2023-04-15 16:34:17 -0300

    feat: add header.php

---
+++ templates/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>pizzaria-php</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="css/styles.css" class="css">
+</head>
+<body>
+  <header>
+    <nav class="navbar navbar-expand-lg">
+      <a href="index.php" class="navbar-brand">
+        <img src="img/pizza.svg" alt="Pizzaria php" id="brand-logo">
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a href="index.php" class="nav-link">Peça sua pizza</a>
+            </li>
+          </ul>
+        </div>
+      </a>
+    </nav>
+  </header>
+  <div class="alert alert-success">
+    <p>Pedido feito com sucesso!</p>
+  </div>
